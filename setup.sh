d367b52fbf6f825d68f8dd8c349281738760bb8e
ram-ch <ch.sairam1988@gmail.com>
2022-12-25 06:36:43 +0000

    added shell script to automate downloads

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# download the Penn-Fudan dataset
+wget https://www.cis.upenn.edu/~jshi/ped_html/PennFudanPed.zip .
+# extract it in the current folder
+unzip PennFudanPed.zip
+rm PennFudanPed.zip
+
+pip install albumentations==0.4.6 pycocotools --quiet
+
+# Clone TorchVision repo and copy helper files
+git clone https://github.com/pytorch/vision.git
+cp vision/references/detection/utils.py ./
+cp vision/references/detection/transforms.py ./
+cp vision/references/detection/coco_eval.py ./
+cp vision/references/detection/engine.py ./
+cp vision/references/detection/coco_utils.py ./
